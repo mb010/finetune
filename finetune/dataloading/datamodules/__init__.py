841ed27bb19cf79d436d6eee1adffd9ed0ede3e2
Inigo Val Slijepcevic <inigoval@gmail.com>
2023-05-23 10:46:04 +0100

    Added MiraBest FITS datamodule (requires astroaugs)

--- finetune/dataloading/datamodules/__init__.py
+++ finetune/dataloading/datamodules/__init__.py
@@ -4,4 +4,5 @@ finetune_datasets = {
     # "stl10": STL10_DataModule,
     # "imagenette": Imagenette_DataModule,
     "rgz": RGZ_DataModule_Finetune,
+    "mb_fits": MiraBest_FITS_DataModule_Finetune,
 }
--- finetune/dataloading/datamodules/rgz.py
+++ finetune/dataloading/datamodules/rgz.py
@@ -22,6 +22,7 @@ from astroaugmentations.datasets.MiraBest_F import (
     MBFRConfident,
     MiraBest_F,
     MBFRUncertain,
+    MiraBest_FITS,
 )
 
 
@@ -161,6 +162,110 @@ class RGZ_DataModule_Finetune(FineTuning_DataModule):
                     train=True,
                     test_size=None,
                     transform=self.T_test,
+                ),
+                idx_val,
+            )
+
+        else:
+            self.data["train"] = MBFRConfident(
+                self.path, aug_type="torchvision", train=True, transform=self.T_train
+            )
+            self.data["val"] = MBFRConfident(
+                self.path, aug_type="torchvision", train=True, transform=self.T_test
+            )
+
+
+def confident_only(df):
+    df = df.loc[df["class"].isin(["FR1", "FR2"])]
+    df = df.loc[df["confidence"] == "confident"]
+    return df.reset_index(drop=True)
+
+
+class MiraBest_FITS_DataModule_Finetune(FineTuning_DataModule):
+    def __init__(self, config):
+        super().__init__(config)
+
+        # Cropping
+        center_crop = config["augmentations"]["center_crop_size"]
+
+        self.T_train = T.Compose(
+            [
+                T.RandomRotation(180),
+                T.CenterCrop(center_crop),
+                T.RandomResizedCrop(center_crop, scale=(0.9, 1)),
+                T.RandomHorizontalFlip(),
+                T.RandomVerticalFlip(),
+                T.ToTensor(),
+                T.Normalize(self.mu, self.sig),
+            ]
+        )
+
+        self.T_test = T.Compose(
+            [
+                T.CenterCrop(center_crop),
+                T.ToTensor(),
+                T.Normalize(self.mu, self.sig),
+            ]
+        )
+
+    def prepare_data(self):
+        MiraBest_F(self.path, train=None, download=True)
+
+    def setup(self, stage=None):
+        # Get test set which is held out and does not change
+        # self.data["test"] = MBFRConfident(
+        #     self.path,
+        #     aug_type="torchvision",
+        #     train=False,
+        #     # test_size=self.config["finetune"]["test_size"],
+        #     test_size=None,
+        #     transform=self.T_test,
+        # )
+
+        self.data["test"] = OrderedDict(
+            {
+                "MB_conf_test": MiraBest_FITS(
+                    self.path,
+                    train=False,
+                    test_size=None,
+                    transform=self.T_test,
+                    df_filter=confident_only,
+                ),
+            },
+        )
+
+        if self.config["finetune"]["val_size"] != 0:
+            data = MiraBest_FITS(
+                self.path,
+                train=True,
+                df_filter=confident_only,
+            )
+            idx = np.arange(len(data))
+            idx_train, idx_val = train_test_split(
+                idx,
+                test_size=self.config["finetune"]["val_size"],
+                stratify=data.full_targets,
+                random_state=self.config["finetune"]["seed"],
+            )
+
+            self.data["train"] = Subset(
+                MiraBest_FITS(
+                    self.path,
+                    train=True,
+                    test_size=None,
+                    transform=self.T_train,
+                    df_filter=confident_only,
+                ),
+                idx_train,
+            )
+
+            self.data["val"] = Subset(
+                MiraBest_FITS(
+                    self.path,
+                    train=True,
+                    test_size=None,
+                    transform=self.T_test,
+                    df_filter=confident_only,
                 ),
                 idx_val,
             )
